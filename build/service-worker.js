79c2b26ea10b74dcf0a24c78387b8439423a2091
Jesse Kevon <jessekevon@gmail.com>
2023-03-31 17:23:45 -0500

    build

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.37a4965ec4cddec642dbca8299830776.js"
+  "./precache-manifest.8e459d209c4c480c83ede05fad2f1333.js"
 );
 
 self.addEventListener('message', (event) => {
