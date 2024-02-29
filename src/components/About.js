129a22fbf90b5c33f75ef6238205b61e52e138d1
Jesse Kevon <jessekevon@gmail.com>
2024-02-29 10:50:53 -0600

    redesign

--- src/components/About.js
+++ src/components/About.js
@@ -13,13 +13,12 @@ class About extends Component {
     return (
       <section className="about">
         <div className="col-md-12">
-          <h2 className="section-title">About</h2>
           <div className="row center mx-auto mb-5">
-            <div className="col-md-8 center">
+            <div className="col-md-10 center">
               <div className="col-md-10">
                 <p>I am a senior-level technologist and web developer with over a decade of experience, adept at seamlessly blending design and development. My expertise lies in creating custom, responsive, and dynamic digital solutions, from marketing sites and web apps to email templates and CMS experiences.</p>
                 <br/>
-                <p>Proficient in technologies like React, HTML5, Sass, Javascript, and PHP, I take pride in delivering visually appealing, efficient, and effective projects. My comprehensive approach covers planning, design, prototyping, and coding, resulting in user-friendly interfaces that are both attractive and well-documented. With a diverse portfolio, I've collaborated with clients across various industries, offering tailored solutions to meet their specific goals. As a creative problem-solver, I thrive on challenging projects and consistently deliver high-quality results. Whether developing a new website or enhancing an existing one, I bring expertise, dedication, and passion to every project.</p>
+                <p>Proficient in technologies like React, HTML5, Sass, Javascript, and PHP, I take pride in delivering visually appealing, efficient, and effective projects. My comprehensive approach covers planning, design, prototyping, and coding, resulting in user-friendly interfaces that are both attractive and well-documented. With a diverse portfolio, I have collaborated with clients across various industries, offering tailored solutions to meet their specific goals. As a creative problem-solver, I thrive on challenging projects and consistently deliver high-quality results. Whether developing a new website or enhancing an existing one, I bring expertise, dedication, and passion to every project.</p>
               </div>
             </div>
           </div>
